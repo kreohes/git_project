7a2b4e533dc5f2bff295fa705803b003b52a032a
kadkin.i <kadkini@yandex.ru>
2022-11-21 17:04:36 +0500

    new lines

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 def main():
     print('My first git program')
     print('And I change it every day')
+    print('And I change it every day')
+    print('My first git program')
 
 
 if __name__ == '__main__':
